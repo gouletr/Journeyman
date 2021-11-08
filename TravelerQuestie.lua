8ab5f383274e5f1a31568bc42fd3ab2ae86407cc
Robert Goulet <robert.goulet.qc@gmail.com>
2021-11-07 20:42:48 -0500

    Fix event objective location

--- TravelerQuestie.lua
+++ TravelerQuestie.lua
@@ -162,12 +162,18 @@ local function GetNearestQuestLocation(entities)
             nearest = GetNearestNPC(entity.NPC, player)
         elseif entity.GameObject then
             nearest = GetNearestObject(entity.GameObject, player)
+        elseif entity.Item then
+            nearest = GetNearestItem(entity.Item, player)
         elseif entity.Type == "monster" then
             nearest = GetNearestNPC({ entity.Id }, player)
         elseif entity.Type == "object" then
             nearest = GetNearestObject({ entity.Id }, player)
         elseif entity.Type == "item" then
             nearest = GetNearestItem({ entity.Id }, player)
+        elseif entity.Type == "event" and entity.Coordinates then
+            nearest = GetNearestSpawn(entity.Coordinates, player)
+            nearest.name = entity.Text
+            nearest.type = "Event"
         end
 
         if nearest and (nearest.distance < bestDistance) then
@@ -252,12 +258,16 @@ function DataSourceQuestie:GetQuestObjectives(questId)
                     GetQuestObjectivesNPC(objectives, objective.NPC)
                 elseif objective.GameObject then
                     GetQuestObjectivesObject(objectives, objective.GameObject)
+                elseif objective.Item then
+                    GetQuestObjectivesItem(objectives, objective.Item)
                 elseif objective.Type == "monster" then
                     GetQuestObjectivesNPC(objectives, { objective.Id })
                 elseif objective.Type == "object" then
                     GetQuestObjectivesObject(objectives, { objective.Id })
                 elseif objective.Type == "item" then
                     GetQuestObjectivesItem(objectives, { objective.Id })
+                elseif objective.Type == "event" then
+                    tinsert(objectives, { name = objective.Text, type = "Event" })
                 end
             end
         end
