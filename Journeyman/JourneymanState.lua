f0bf14862853238dffce124d505794599ec6bcbd
Robert Goulet <robert.goulet.qc@gmail.com>
2022-10-02 12:04:27 -0400

    Fix availability of taxi nodes

--- Journeyman/JourneymanState.lua
+++ Journeyman/JourneymanState.lua
@@ -481,7 +481,7 @@ function State:IsStepAvailable(step)
     if Journeyman:IsStepTypeQuest(step) then
         return self:IsQuestAvailable(step.data.questId)
     elseif step.type == Journeyman.STEP_TYPE_FLY_TO then
-        return TaxiNodes:IsAvailable(step.data.taxiNodeId)
+        return TaxiNodes:IsAvailable(step.data.taxiNodeId, Journeyman.player.faction)
     end
 
     return true
@@ -493,7 +493,7 @@ function State:IsStepDoable(step)
     elseif step.type == Journeyman.STEP_TYPE_BIND_HEARTHSTONE or step.type == Journeyman.STEP_TYPE_USE_HEARTHSTONE then
         return DataSource:GetNearestInnkeeperLocation(step.data.areaId) ~= nil
     elseif step.type == Journeyman.STEP_TYPE_LEARN_FLIGHT_PATH then
-        return TaxiNodes:IsAvailable(step.data.taxiNodeId)
+        return TaxiNodes:IsAvailable(step.data.taxiNodeId, Journeyman.player.faction)
     elseif step.type == Journeyman.STEP_TYPE_FLY_TO then
         return Journeyman.db.char.taxiNodeIds[step.data.taxiNodeId] == true
     end
